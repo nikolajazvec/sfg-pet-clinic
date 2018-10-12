f6c498d58fece051985b2f8edf71123961176a9b
Nikola Jazvec <Nikola.Jazvec@corvus.hr>
2018-10-12 14:48:47 +0200

    Initial project commit

---
+++ src/main/java/amateur/springframework/sfppetclinic/SfpPetClinicApplication.java
@@ -0,0 +1,12 @@
+package amateur.springframework.sfppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfpPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfpPetClinicApplication.class, args);
+	}
+}
