74e3995ea1fac101a2475a6ea070bb4a862e62ef
Nikola Jazvec <Nikola.Jazvec@corvus.hr>
2018-10-19 13:36:21 +0200

    Progress

--- pet-clinic-web/src/main/java/amateur/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/amateur/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -22,25 +22,21 @@ public class DataLoader implements CommandLineRunner{
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Nikola");
         owner1.setLastName("Jazvec");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Nikolica");
         owner2.setLastName("Prikolica");
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Ivica");
         vet1.setLastName("Ruski");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Ivica");
         vet2.setLastName("Debeji");
         vetService.save(vet2);
